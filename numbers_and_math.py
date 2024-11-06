bc40c662f04b45c47b26401a19b169549b1ea68f
Natalie Wong <natnatwong531@gmail.com>
2024-11-06 12:14:25 -0500

    Create numbers_and_math.py

---
+++ numbers_and_math.py
@@ -0,0 +1,8 @@
+#numbers of apple
+apple = 17
+print(f"I have {apple} apples.")
+#number of the apple I ate
+ate = 3
+print("I ate 3 yesterday. ")
+print(f"there are {apple - ate} apple left. ")
+print()
