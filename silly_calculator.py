ad6913f8e9c8e5e100ed79ebcf0cebb904bca1d6
Natalie Wong <natnatwong531@gmail.com>
2024-11-19 09:23:07 -0500

    Create silly_calculator.py

---
+++ silly_calculator.py
@@ -0,0 +1,8 @@
+print("What is your first number?")
+first = float(input())
+print("What is your second number?")
+second = float(input())
+print("What is your third number?")
+third = float(input())
+answer = (first + second + third) / 2 
+print(f"{first} + {second} + {third} / 2 is... {answer}")
