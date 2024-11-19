29c3485eaf9ed4af39f9ed71e7ee8b66d076aed3
Natalie Wong <natnatwong531@gmail.com>
2024-11-19 09:15:49 -0500

    Create age_in_five_years.py

---
+++ age_in_five_years.py
@@ -0,0 +1,8 @@
+print("Hello.  What is your name? ")
+name = input()
+print(f"Hi, {name}! How old are you? ")
+age = int(input())
+in_five_years = age + 5
+five_years_ago = age - 5
+print(f"Did you know that in five years you will be {in_five_years} years old? ")
+print(f"And five years ago you were {five_years_ago}! Imagine that! ")
